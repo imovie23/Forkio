5dc52079d6ca572aa8e8135df0397bff12e483a9
Misha Starodubets <starodubetsm@gmail.com>
2019-08-20 13:42:44 +0300

    Updates

--- js/components/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.toggle');
-const headerNav = document.querySelector('.header__nav');
-
-navToggle.addEventListener('click', handleToggle);
-
-function handleToggle() {
-	headerNav.classList.toggle('is-open');
-}
