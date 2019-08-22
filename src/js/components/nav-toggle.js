36230dae87b982cf100340d407a507c2310cc8b6
Misha Starodubets <starodubetsm@gmail.com>
2019-08-22 14:08:32 +0300

    fixed

--- src/js/components/nav-toggle.js
+++ src/js/components/nav-toggle.js
@@ -1,8 +1,23 @@
 const navToggle = document.querySelector('.toggle');
 const headerNav = document.querySelector('.header__nav');
 
-navToggle.addEventListener('click', handleToggle);
-
 function handleToggle() {
 	headerNav.classList.toggle('is-open');
+}
+
+const mqList = window.matchMedia('(min-width: 481px)');
+mqList.addListener(screenTest);
+screenTest(mqList);
+
+function screenTest(e) {
+	if (e.matches) {
+		navToggle.removeEventListener('click', handleToggle);
+
+		if (headerNav.classList.contains('is-open')) {
+			headerNav.classList.remove('is-open');
+		}
+
+	} else {
+		navToggle.addEventListener('click', handleToggle);
+	}
 }
